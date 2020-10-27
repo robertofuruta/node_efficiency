7a320127297769de1e1acbc3c56bd1dabc66f942
Roberto Furuta <robertofuruta@Robertos-MacBook-Pro.local>
2020-10-26 22:09:57 -0300

    Calculating the eff and printing to the terminal, tested working correctly with the first example

--- node_eff.cpp
+++ node_eff.cpp
@@ -8,6 +8,43 @@
 #include <sstream>
 using namespace std;
 
+bool breadth_first_search(vector<int> adj_list[], int src, int N, int dist[])
+{
+    list<int> queue;            // list of nodes not evaluated yet
+    bool visited[N];            // each position represents each node, it stores if the node was visited at least once
+    for (int i = 0; i < N; i++) //initializing each array
+    {
+        visited[i] = false;
+        dist[i] = INT_MAX;
+        // pred[i] = -1;
+    }
+
+    // updating the first node: src
+    visited[src] = true;
+    dist[src] = 0;
+    queue.push_back(src);
+
+    // standard BFS algorithm
+    while (!queue.empty())
+    {                             //while there are nodes to evaluate
+        int node = queue.front(); //the first node is chosen
+        queue.pop_front();        //and it is removed from the queue
+
+        for (int i = 0; i < adj_list[node].size(); i++)
+        { //for each index of neighbour
+            if (visited[adj_list[node][i]] == false)
+            {                                             // if it's not been visited:
+                visited[adj_list[node][i]] = true;        // update to visited
+                dist[adj_list[node][i]] = dist[node] + 1; // +1 step distance
+                // pred[adj_list[node][i]] = node;           // register the predecessor
+                queue.push_back(adj_list[node][i]); // add to the queue
+            }
+        }
+    }
+
+    return false; // if was't able to arrive at the destination.
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2) //Reads the program name and the .edgelist file name from the terminal
@@ -53,7 +90,28 @@ int main(int argc, char *argv[])
     };
 
     // debug: print each neighbour pair
+    // for (int i = 0; i < N; i++)
+    //     for (int j = 0; j < adj_list[i].size(); j++)
+    //         cout << "From node " << i << " to node " << adj_list[i][j] << endl;
+
+    int dist_mat[N][N]; //stores the distances from src
     for (int i = 0; i < N; i++)
-        for (int j = 0; j < adj_list[i].size(); j++)
-            cout << "From node " << i << " to node " << adj_list[i][j] << endl;
+    {
+        breadth_first_search(adj_list, i, N, dist_mat[i]); //gets the distance from node i to all the nodes
+    }
+
+    double eff_list[N]; // stores the efficiency of each node
+    for (int i = 0; i < N; i++)
+    {
+        cout << "node eff " << i << endl;
+        eff_list[i] = 0; //initializes as 0.
+        for (int j = 0; j < N; j++)
+        {
+            if (i != j && dist_mat[i][j] != INT_MAX) // if not the source node itself, neither isolated:
+            {
+                eff_list[i] += 1. / dist_mat[i][j] / (N - 1);
+            }
+        }
+        cout << eff_list[i] << endl;
+    }
 }
