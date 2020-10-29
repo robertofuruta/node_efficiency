fcbc4617202d0c73261c796e6919ed16f0c050d3
Roberto Furuta <robertofuruta@Robertos-MacBook-Pro.local>
2020-10-29 13:36:02 -0300

    Cleaning up.

--- bach_process.py
+++ bach_process.py
@@ -2,6 +2,21 @@ import os
 import glob
 from cd import cd
 
+
+class cd:
+    """Context manager for changing the current working directory"""
+
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+
 with cd('test-data-eff'):
     edgelist_list = glob.glob('*edgelist')
     print(edgelist_list)
